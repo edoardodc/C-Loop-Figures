4b7bce7ec74c58d7122b234a6e2ecfa5c8ac6142
riclapi <37591236+riclapi@users.noreply.github.com>
2019-04-02 21:35:13 +0200

    Update main.c

--- ProgettoScuola/ProgettoScuola/main.c
+++ ProgettoScuola/ProgettoScuola/main.c
@@ -20,13 +20,13 @@ int main() {
         char buff[100];
         char scelta, *triangolo, *quadrato, *rettangolo, *ellisse, *rombo, *casa, *clessidra;
         int  entraNellaConclusione = 1;
-        
+
         printf("Ecco le figure che oggi abbiamo a disposizione:\n\n - Rettangolo\n - Clessidra\n - Triangolo\n - Quadrato\n - Ellisse\n - Rombo\n - Casa\n\n");
         printf("Inserire nome figura: ");
         scanf("%s", buff);
-        
+
         system("cls");
-        
+
         triangolo = strstr(buff, "triangolo");
         quadrato = strstr(buff, "quadrato");
         rettangolo = strstr(buff, "rettangolo");
@@ -34,7 +34,7 @@ int main() {
         rombo = strstr(buff, "rombo");
         casa = strstr(buff, "casa");
         clessidra = strstr(buff, "clessidra");
-        
+
         if (triangolo != NULL) {
             mostraMenuTriangoli();
         } else if (quadrato != NULL) {
@@ -53,12 +53,12 @@ int main() {
             printf("%s che buffo nome! Temo che non sia in elenco!\n", buff);
             entraNellaConclusione = -1;
         }
-        
+
         if (entraNellaConclusione == 1) {
             printf("\n");
             printf("Fantastico! premere qualsiasi tasto se vuoi continuare l'esperienze oppure premere 2 se vuoi concluderla: ");
             scanf(" %c", &scelta);
-            
+
             if (scelta == '2') {
                 break;
             }
@@ -73,17 +73,17 @@ void mostraMenuQuadrato() {
     char  buff[1000];
     char *pieno, *vuoto, *particolare, *numerato, *farcito;
     printf("Fantastico! Ecco qui i tipi di quadrati:\n\n - Pieno\n - Vuoto\n - Particolare\n - Numerato \n - Farcito\n\n");
-    
+
     do {
         printf("Inserire tipo di quadrato tra quelli elencati: ");
         scanf("%s",buff);
-        
+
         vuoto = strstr(buff, "vuoto");
         pieno = strstr(buff, "pieno");
         particolare = strstr(buff, "particolare");
         numerato = strstr(buff, "numerato");
         farcito = strstr(buff, "farcito");
-        
+
         if (pieno != NULL) {
             creaQuadratoPieno();
             break;
@@ -109,17 +109,17 @@ void mostraMenuQuadrato() {
 void mostraMenuRettangolo() {
     char  buff[100];
     char *pieno, *vuoto, *particolare;
-    
+
     printf("Fantastico! Ecco qui i tipi di rettangoli:\n\n - Pieno\n - Vuoto\n - Particolare\n\n");
-    
+
     do {
         printf("Inserire tipo di rettangolo tra quelli elencati: ");
         scanf("%s",buff);
-        
+
         pieno = strstr(buff, "pieno");
         vuoto = strstr(buff, "vuoto");
         particolare = strstr(buff, "particolare");
-        
+
         if (pieno != NULL) {
             creaRettangoloPieno();
             break;
@@ -139,16 +139,17 @@ void mostraMenuRettangolo() {
 void mostraMenuTriangoliEquilatero() {
     char  buff[100];
     char *vuoto, *pieno;
-    
+    system("cls");
+
     printf("Fantastico! Ecco qui i tipi di triangoli equilateri:\n\n - Vuoto\n - Pieno\n\n");
-    
+
     do {
         printf("Inserire tipo di triangoli equilatero tra quelli elencati: ");
         scanf("%s",buff);
-        
+
         vuoto = strstr(buff, "vuoto");
         pieno = strstr(buff, "pieno");
-        
+
         if (pieno != NULL) {
             creaTriangoloEquilatero();
             break;
@@ -165,16 +166,16 @@ void mostraMenuTriangoliEquilatero() {
 void mostraMenuTriangoliRettangoli() {
     char  buff[100];
     char *vuoto, *pieno;
-    
+    system("cls");
     printf("Fantastico! Ecco qui i tipi di triangoli rettangoli:\n\n - Vuoto\n - Pieno\n\n");
-    
+
     do {
         printf("Inserire tipo di triangolo rettangolo tra quelli elencati: ");
         scanf("%s",buff);
-        
+
         vuoto = strstr(buff, "vuoto");
         pieno = strstr(buff, "pieno");
-        
+
         if (pieno != NULL) {
             creaTriangoloRettangolo();
             break;
@@ -191,17 +192,17 @@ void mostraMenuTriangoliRettangoli() {
 void mostraMenuTriangoli() {
     char  buff[100];
     char *trRettangolo, *equilatero, *numerato;
-    
+
     printf("Fantastico! Ecco qui i tipi di triangoli:\n\n - Rettangolo\n - Equilatero\n - Numerato\n\n");
-    
+
     do {
         printf("Inserire tipo di triangoli tra quelli elencati: ");
         scanf("%s",buff);
-        
+
         equilatero = strstr(buff, "equilatero");
         trRettangolo = strstr(buff, "rettangolo");
         numerato = strstr(buff, "numerato");
-        
+
         if (equilatero != NULL) {
             mostraMenuTriangoliEquilatero();
             break;
